6ca890281215ccffa980749b4759f9696b7b92c8
Danny Aslam-Perez <danny@intricatecloud.io>
2023-03-06 17:35:06 -0500

    Uploading exercise after class

--- index.js
+++ index.js
@@ -1,5 +1,58 @@
-const database = {}
+let database = {
+    classes: [],
+    questions: []
+}
 
-export const getQuestions = () => {
-    throw new Error('Not implemented yet')
+const getRandomNumber = () => {
+    return `${Math.floor(Math.random() * 900000) + 100000}`
+}
+
+export const createClass = (className) => {
+    database.classes.push({
+        className: className
+    })
+}
+
+export const getClasses = () => {
+    return database.classes
+}
+
+export const createClassSession = className => {
+    // find the class with the class name
+    const classInfo = database.classes.find(classInfo => {
+        return classInfo.className === className
+    })
+
+    classInfo.sessions = []
+    classInfo.sessions.push(getRandomNumber())
+}
+
+export const getClassSessions = className => {
+    // find the class with the class name
+    const classInfo = database.classes.find(classInfo => {
+        return classInfo.className === className
+    })
+    return classInfo.sessions;
+}
+
+export const askQuestion = (name, question, classSession) => {
+    database.questions.push({
+        name: name,
+        question: question,
+        classSession: classSession
+    })
+}
+
+export const getQuestions = (classSession) => {
+    // find the question for a class session
+    return database.questions.filter(question => {
+        return question.classSession === classSession
+    });
+}
+
+export const clear = () => {
+    database = {
+        classes: [],
+        questions: []
+    }
 }
--- index.spec.js
+++ index.spec.js
@@ -1,8 +1,51 @@
 import * as db from './index.js'
 
 describe('class questions app', () => {
-    it('gets a question', () => {
-        const questions = db.getQuestions()
-        expect(questions).toEqual([])
+    afterEach(() => {
+        db.clear();
+    })
+
+    describe('Classes', () => {
+        it('creates a class', () => {
+            const className = 'CS 5356'
+            db.createClass(className)
+    
+            const classes = db.getClasses()
+            expect(classes.length).toEqual(1)
+            expect(classes[0]).toEqual({className: 'CS 5356'})
+        })
+    
+        it('creates a class session for a class', () => {
+            const className = 'CS 5356'
+            db.createClass(className)
+    
+            db.createClassSession(className)
+    
+            const classSessions = db.getClassSessions(className)
+            expect(classSessions.length).toEqual(1)
+        })    
+    })
+
+    it('asks a question', () => {
+        const className = 'CS 5356'
+        db.createClass(className)
+
+        db.createClassSession(className)
+        db.createClassSession(className)
+        const classSessions = db.getClassSessions(className)
+
+        let name = 'user123'
+        let question = 'Why is the sky blue?'
+        let classSession = classSessions[0]
+        db.askQuestion(name, question, classSession)
+
+        name = 'user456'
+        question = 'A very random question'
+        classSession = classSessions[1]
+        db.askQuestion(name, question, classSession)
+
+        const questions = db.getQuestions(classSessions[0])
+        expect(questions.length).toEqual(1)
+        expect(questions[0].question).toEqual('Why is the sky blue?')
     })
 })
